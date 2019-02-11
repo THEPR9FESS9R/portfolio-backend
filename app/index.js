66b4049e9c628d860daf91652f4545a228465c56
Pascal Korn <Pascal.Korn@meinestadt.de>
2019-02-11 21:32:44 +0100

    stuff

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log('Hello World!')
-    res.send('Hello World!')
+    console.log('Hello World!');
+    // res.send('Hello World!')
+    res.sendFile(path.resolve('./index.html'));
 });
 
 app.listen(80, () => {
