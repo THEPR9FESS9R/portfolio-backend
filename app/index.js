58955ec63cc922fec20eb55f205d7047f25284b7
Pascal Korn <Pascal.Korn@meinestadt.de>
2019-02-12 20:24:39 +0100

    stuff

--- app/index.js
+++ app/index.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const path = require('path');
+const http2 = require('http2');
+const fs = require('fs');
 
-const app = express();
+const server = http2.createSecureServer({
+    key: fs.readFileSync(path.resolve('./certs/localhost.key')),
+    cert: fs.readFileSync(path.resolve('./certs/localhost.crt'))
+  });
+  server.on('error', (err) => console.error(err));
+  
+  server.on('stream', (stream, headers) => {
+    // stream is a Duplex
+    console.log('hit');
+    stream.respond({
+      'content-type': 'text/html',
+      ':status': 200
+    });
+    let readStream = fs.createReadStream(path.resolve('./index.html'));
+    readStream.pipe(stream);
+    // stream.end('<h1>Hello World</h1>');
+  });
+  
+  console.log('https://localhost:8089');
+  server.listen(8089, '127.0.0.1');
 
-app.get('/', (req, res) => {
-    console.log('Hello World!');
-    // res.send('Hello World!')
-    res.sendFile(path.resolve('./index.html'));
-});
+// const app = express();
 
-app.listen(80, () => {
-});
+// app.get('/', (req, res) => {
+//     console.log('Hello World!');
+//     // res.send('Hello World!')
+//     res.sendFile(path.resolve('./index.html'));
+// });
+
+// app.listen(80, () => {
+// });
