5cf9b7b5e17aa9c79bfb29ba2716811556179904
Pascal Korn <Pascal.Korn@meinestadt.de>
2019-02-15 09:43:34 +0100

    stuff

--- app/index.js
+++ app/index.js
@@ -1,28 +1,42 @@
-const express = require('express');
-const path = require('path');
-const http2 = require('http2');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const http2 = require("http2");
+const fs = require("fs");
 
 const server = http2.createSecureServer({
-    key: fs.readFileSync(path.resolve('./certs/localhost.key')),
-    cert: fs.readFileSync(path.resolve('./certs/localhost.crt'))
+  allowHTTP1: true,
+  key: fs.readFileSync(path.resolve("./certs/localhost.key")),
+  cert: fs.readFileSync(path.resolve("./certs/localhost.crt"))
+});
+
+// server.on('request', (req, res) => {
+//   // detects if it is a HTTPS request or HTTP/2
+//   const { socket: { alpnProtocol } } = req.httpVersion === '2.0' ?
+//     req.stream.session : req;
+//   res.writeHead(200, { 'content-type': 'application/json' });
+//   res.end(JSON.stringify({
+//     alpnProtocol,
+//     httpVersion: req.httpVersion
+//   }));
+// });
+
+server.on("error", err => console.error(err));
+
+server.on("stream", (stream, headers) => {
+  // stream is a Duplex
+  console.log("hit");
+  stream.respond({
+    "content-type": "text/html",
+    ":status": 200
   });
-  server.on('error', (err) => console.error(err));
-  
-  server.on('stream', (stream, headers) => {
-    // stream is a Duplex
-    console.log('hit');
-    stream.respond({
-      'content-type': 'text/html',
-      ':status': 200
-    });
-    let readStream = fs.createReadStream(path.resolve('./index.html'));
-    readStream.pipe(stream);
-    // stream.end('<h1>Hello World</h1>');
-  });
-  
-  console.log('https://localhost:8089');
-  server.listen(8089, '127.0.0.1');
+  let readStream = fs.createReadStream(path.resolve("./index.html"));
+  readStream.pipe(stream);
+  // stream.end('<h1>Hello World</h1>');
+});
+const port = 8089;
+// const ip = '0.0.0.0';
+// console.log(`https://${ip}:${port}`);
+server.listen(port);
 
 // const app = express();
 
